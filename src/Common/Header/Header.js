c519569c4a6f3309f97c9e2f9527c942305bd4d0
Anil Nair <aniln@vmware.com>
2020-02-14 20:28:50 +0530

    add common folder and start header code

---
+++ src/Common/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = function(){
+    return(
+        <div className="header">
+            <div className="title" >Image Viewer
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
 ReactDOM.render(
     <span>
-        Image Viewer
+        <App/>
     </span>, 
     document.getElementById('root')
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
